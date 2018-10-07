edec51c36f0f2d274e13b7c1b4efa2d503bb11e8
olivan <olivandesign@gmail.com>
2018-10-07 11:04:29 +0300

    finished new logic

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -17,17 +17,16 @@ export default class Game extends React.PureComponent {
       currentStepValue: 'X',
       xIsNext: false,
       winner: null,
-      isGameOver: false,
     };
   }
 
   handleSquareClick = (rowId, columnId) => {
-    const { history, squares, currentStepValue, xIsNext, winner, isGameOver } = this.state;
+    const { history, squares, currentStepValue, xIsNext, winner } = this.state;
     const newHistory = [...history];
     const newSquares = [...squares].map(row => [...row]);
     const isSquareFilled = squares[rowId][columnId];
 
-    if (!(isSquareFilled || isGameOver)) {
+    if (!(isSquareFilled || winner)) {
       newSquares[rowId][columnId] = currentStepValue;
       newHistory.push(this.state);
       this.setState({
@@ -36,16 +35,11 @@ export default class Game extends React.PureComponent {
         currentStepValue: currentStepValue,
         xIsNext: xIsNext,
         winner: winner,
-        isGameOver: isGameOver,
       }, () => {
         this.setNextStep(rowId, columnId)
       });
     }
     return null;
-  }
-
-  handleHistoryClick = id => {
-    this.setState(prevState => prevState.history[id]);
   }
 
   setNextStep = (rowId, columnId) => {
@@ -58,35 +52,39 @@ export default class Game extends React.PureComponent {
       currentStepValue: xIsNext ? 'X' : 'O',
       xIsNext: !xIsNext,
       winner: winner ? winner : null,
-      isGameOver: winner ? true : false,
     });
 
     return null;
   }
 
   calculateWinner = (rowId, columnId) => {
-    return (this.checkRow(rowId) || this.checkColumn(columnId));
-  }
-
-  checkRow = rowId => {
-    const row = this.state.squares[rowId];
-  
-    return row.reduce((a, b) => a === b ? b : null);
-  }
-
-  checkColumn = columnId => {
     const { squares } = this.state;
+    const row = this.state.squares[rowId];
     const column = [];
+    const diagonalLeft = [];
+    const diagonalRight = [];
 
-    for (let i = 0; i < boardSize; i++) {
+    for (let i = 0; i < boardSize; i++ ) {
       column.push(squares[i][columnId]);
+      diagonalLeft.push(squares[i][i]);
+      diagonalRight.push(squares[i][(boardSize - 1) - i]);
     }
 
-    return column.reduce((a, b) => a === b ? b : null);
+    return (this.checkLine(row) || 
+            this.checkLine(column) || 
+            this.checkLine(diagonalLeft) ||
+            this.checkLine(diagonalRight));
   }
 
-  checkCrosses = (rowId, columnId) => {
+  checkLine = line => {
+    if (line.length > 0) {
+      return line.reduce((a, b) => a === b ? b : null);
+    }
+    return null;
+  }
 
+  handleHistoryClick = id => {
+    this.setState(prevState => prevState.history[id]);
   }
 
   render() {
